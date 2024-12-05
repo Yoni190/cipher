afa38b4c32d48f901e79e23bfec0f3b8836b9bc6
Yoni190 <yfisuhu@gmail.com>
2024-12-05 12:38:29 +0300

    Make the letter start from a if reached the end

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let button = document.getElementById("decipher");
-const downletter = "abcdefghijklmnopqrstuvwxyz";
+const downLetter = "abcdefghijklmnopqrstuvwxyz";
 
 button.addEventListener("click", () =>{
     let code = document.getElementById("code").value;
@@ -7,9 +7,19 @@ button.addEventListener("click", () =>{
     let deciphered = [];
 
     for(let i = 0; i < code.length; i++){
-        let shiftedIndex = downletter.indexOf(code[i]) + shift;
-        let shiftedLetter = downletter[shiftedIndex];
-        deciphered.push(shiftedLetter);
+        if(code[i] == " "){
+            deciphered.push(" ");
+        }
+        else{
+            let shiftedIndex = downLetter.indexOf(code[i]) + shift;
+            //This makes the alphabet a loop
+            if (shiftedIndex > 25){
+                shiftedIndex -= 26;
+            }
+            let shiftedLetter = downLetter[shiftedIndex];
+            deciphered.push(shiftedLetter);
+        }
     }
    alert(deciphered.join(""));
+
 });
