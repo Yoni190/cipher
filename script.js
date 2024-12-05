b750ab41c8ab27a6009e5682e642ae4b42095e5f
Yoni190 <yfisuhu@gmail.com>
2024-12-05 12:51:28 +0300

    Decipher uppercase letters

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let button = document.getElementById("decipher");
 const downLetter = "abcdefghijklmnopqrstuvwxyz";
+const capitalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 button.addEventListener("click", () =>{
     let code = document.getElementById("code").value;
@@ -7,16 +8,23 @@ button.addEventListener("click", () =>{
     let deciphered = [];
 
     for(let i = 0; i < code.length; i++){
+        let shiftedIndex = 0;
         if(code[i] == " "){
             deciphered.push(" ");
         }
         else{
-            let shiftedIndex = downLetter.indexOf(code[i]) + shift;
+            //Check if char is upper or lower case
+            if(code[i] === code[i].toUpperCase()){
+                shiftedIndex = capitalLetter.indexOf(code[i]) + shift;
+            }
+            else{
+                shiftedIndex = downLetter.indexOf(code[i]) + shift;
+            }
             //This makes the alphabet a loop
             if (shiftedIndex > 25){
                 shiftedIndex -= 26;
             }
-            let shiftedLetter = downLetter[shiftedIndex];
+            let shiftedLetter = code[i] == code[i].toUpperCase() ? capitalLetter[shiftedIndex] : downLetter[shiftedIndex];
             deciphered.push(shiftedLetter);
         }
     }
